4d84c5c39640789a7b145cdccca249f2ea143020
Akira Sugiura <urasandesu@gmail.com>
2012-03-04 17:07:54 +0900

    Filter Files To Make Foundation
    Remove temporarily unnecessary files to make foundation of Prig.
    Add minimum file set to be made success for the unit tests.

--- Urasandesu.NAnonym/Mixins/System/TypeMixin.cs
+++ Urasandesu.NAnonym/Mixins/System/TypeMixin.cs
@@ -1,163 +1,75 @@
-/* 
- * File: TypeMixin.cs
- * 
- * Author: Akira Sugiura (urasandesu@gmail.com)
- * 
- * 
- * Copyright (c) 2010 Akira Sugiura
- *  
- *  This software is MIT License.
- *  
- *  Permission is hereby granted, free of charge, to any person obtaining a copy
- *  of this software and associated documentation files (the "Software"), to deal
- *  in the Software without restriction, including without limitation the rights
- *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- *  copies of the Software, and to permit persons to whom the Software is
- *  furnished to do so, subject to the following conditions:
- *  
- *  The above copyright notice and this permission notice shall be included in
- *  all copies or substantial portions of the Software.
- *  
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- *  THE SOFTWARE.
- */
- 
-
-using System;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using Urasandesu.NAnonym.Mixins.System.Reflection;
-using Urasandesu.NAnonym.ILTools;
-using Urasandesu.NAnonym.ILTools.Impl.System.Reflection;
+using System.Runtime.Serialization;
 
 namespace Urasandesu.NAnonym.Mixins.System
 {
     public static class TypeMixin
     {
-        public static MethodInfo GetMethod(this Type type, MethodInfo methodInfo)
+        public static T ForciblyNew<T>(params object[] args)
         {
-            return type.GetMethod(
-                            methodInfo.Name,
-                            methodInfo.ExportBinding(),
-                            null,
-                            methodInfo.GetParameters().Select(parameter => parameter.ParameterType).ToArray(),
-                            null);
+            var bindingAttr = BindingFlags.Public |
+                              BindingFlags.NonPublic |
+                              BindingFlags.Instance;
+            var binder = default(Binder);
+            var types = args == null || args.Length == 0 ?
+                            Type.EmptyTypes :
+                            args.Select(_ => _.GetType()).ToArray();
+            var modifiers = default(ParameterModifier[]);
+            var ctor = typeof(T).GetConstructor(bindingAttr, binder, types, modifiers);
+            return (T)ctor.Invoke(args);
         }
 
-        public static readonly BindingFlags StaticNonPublic = BindingFlags.Static | BindingFlags.NonPublic;
-        public static readonly BindingFlags InstancePublic = BindingFlags.Instance | BindingFlags.Public;
-        public static readonly BindingFlags InstanceNonPublic = BindingFlags.Instance | BindingFlags.NonPublic;
-
-        public static MethodInfo GetMethodStaticNonPublic(this Type type, string name, Type[] types)
+        public static bool IsSerializable(this Type t)
         {
-            return type.GetMethod(name, StaticNonPublic, null, types, null);
-        }
-
-        public static MethodInfo GetMethodInstancePublic(this Type type, string name)
-        {
-            return type.GetMethod(name, InstancePublic, null, Type.EmptyTypes, null);
-        }
-
-        public static MethodInfo GetMethodInstancePublic(this Type type, string name, Type[] types)
-        {
-            return type.GetMethod(name, InstancePublic, null, types, null);
-        }
-
-        public static FieldInfo GetFieldStaticNonPublic(this Type type, string name)
-        {
-            return type.GetField(name, StaticNonPublic);
-        }
-
-        public static FieldInfo GetFieldInstanceNonPublic(this Type type, string name)
-        {
-            return type.GetField(name, InstanceNonPublic);
-        }
-
-        public static bool IsAssignableFrom(this Type source, ITypeDeclaration target)
-        {
-            if (source.Equivalent(target))
-            {
+            if (t == null)
                 return true;
-            }
-            else
-            {
-                return source.IsAssignableFrom(target.BaseType);
-            }
-        }
 
-        public static bool Equivalent(this Type source, ITypeDeclaration target)
-        {
-            if (source == null && target == null) return true;
-            if (source == null || target == null) return false;
-            return source.AssemblyQualifiedName == target.AssemblyQualifiedName;
-        }
-
-        public static bool Equivalent(this Type source, Type target)
-        {
-            if (!source.IsGenericTypeDefinition)
-            {
-                return source == target;
-            }
-            else
-            {
-                return target.Name == source.Name &&
-                       target.IsGenericType &&
-                       target.GetGenericArguments().Length == source.GetGenericArguments().Length &&
-                       target == source.MakeGenericType(target.GetGenericArguments());
-            }
-        }
-
-        public static bool EquivalentWithoutGenericArguments(this Type source, Type target)
-        {
-            if (!source.IsGenericType)
-            {
-                return source == target;
-            }
-            else
-            {
-                return target.Name == source.Name &&
-                       target.IsGenericType &&
-                       target.GetGenericArguments().Length == source.GetGenericArguments().Length;
-            }
-        }
-
-        public static bool EquivalentGenericParameterPosition(this Type source, Type target)
-        {
-            if (!source.IsGenericParameter && !target.IsGenericParameter)
-            {
+            if (t.HasCustomAttributes<SerializableAttribute>())
                 return true;
-            }
-            else if (!source.IsGenericParameter || !target.IsGenericParameter)
-            {
+
+            if (t.EnumerateBaseType().Where(HasCustomAttributes<SerializableAttribute>).
+                                      Where(HasInterface<ISerializable>).
+                                      Any())
+                return true;
+
+            return false;
+        }
+
+        public static bool HasCustomAttributes<TAttribute>(this Type t) where TAttribute : Attribute
+        {
+            if (t == null)
                 return false;
-            }
-            else
-            {
-                return source.GenericParameterPosition == target.GenericParameterPosition;
-            }
+
+            return t.GetCustomAttributes(typeof(TAttribute), false).Any();
         }
 
-        public static bool IsAssignableWithoutGenericArgumentsFrom(this Type source, Type target)
+        public static bool HasInterface<TInterface>(this Type t) where TInterface : class
         {
-            if (!source.IsGenericType || !target.IsGenericType)
-            {
-                return source.IsAssignableFrom(target);
-            }
-            else
-            {
-                return source.GetGenericTypeDefinition().IsAssignableFrom(target.GetGenericTypeDefinition());
-            }
+            if (!HasInterfaceImpl<TInterface>.IsInterface)
+                return false;
+
+            return t.GetInterface(typeof(TInterface).FullName) != null;
         }
 
-        public static ITypeDeclaration ToTypeDecl(this Type source)
+        class HasInterfaceImpl<TInterface>
         {
-            return new SRTypeDeclarationImpl(source);
+            static bool ms_isInterface = typeof(TInterface).IsInterface;
+            public static bool IsInterface { get { return ms_isInterface; } }
+        }
+
+        public static IEnumerable<Type> EnumerateBaseType(this Type t)
+        {
+            if (t == null)
+                yield break;
+
+            yield return t.BaseType;
+            foreach (var baseType in EnumerateBaseType(t.BaseType))
+            {
+                yield return baseType;
+            }
         }
     }
 }
-
